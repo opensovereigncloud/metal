c49702566ff19acf2ec98502c95076c1bebbafb2
Artem Bortnikov <aobort@users.noreply.github.com>
2021-04-27 14:33:13 +0300

    API update

--- api/v1alpha1/switch_types.go
+++ api/v1alpha1/switch_types.go
@@ -20,70 +20,146 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// SwitchSpec defines the desired state of Switch
-// +kubebuilder:object:generate=true
+//SwitchSpec defines the desired state of Switch
+//+kubebuilder:object:generate=true
 type SwitchSpec struct {
-	// ID referring to switch object id
-	// +kubebuilder:validation:Required
-	//todo: +kubebuilder:validation:Pattern=`^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})$`
-	ID string `json:"id"`
-	// Partition referring to switch's location
-	// +kubebuilder:validation:Optional
-	Partition string `json:"partition,omitempty"`
-	// Room referring to switch's location
-	// +kubebuilder:validation:Optional
-	Room string `json:"room,omitempty"`
-	// Row referring to switch's location
-	// +kubebuilder:validation:Optional
-	Row int16 `json:"row,omitempty"`
-	// Rack referring to switch's location
-	// +kubebuilder:validation:Optional
-	Rack int16 `json:"rack,omitempty"`
-	// Ports referring to switch port number
-	// +kubebuilder:validation:Required
+	//Hostname
+	//+kubebuilder:validation:Required
+	Hostname string `json:"hostname"`
+	//Location referring to the switch location
+	//+kubebuilder:validation:Optional
+	Location *LocationSpec `json:"location,omitempty"`
+	//Ports referring to network interfaces total count
+	//+kubebuilder:validation:Required
 	Ports uint64 `json:"ports"`
-	// NeighboursCount referring to switch's neighbours
-	// +kubebuilder:validation:Optional
-	NeighboursCount uint8 `json:"neighboursCount,omitempty"`
-	// Neighbours referring to switch's neighbours
-	// +kubebuilder:validation:Optional
-	Neighbours []NeighbourSpec `json:"neighbours,omitempty"`
+	//SwitchPorts referring to non-management network interfaces count
+	//+kubebuilder:validation:Required
+	SwitchPorts uint64 `json:"switchPorts"`
+	//SwitchDistro referring to switch OS information
+	//+kubebuilder:validation:Optional
+	SwitchDistro *SwitchDistroSpec `json:"switchDistro,omitempty"`
+	//SwitchChassis referring to switch hardware information
+	//+kubebuilder:validation:Required
+	SwitchChassis *SwitchChassisSpec `json:"switchChassis"`
+	//SouthSubnet referring to south subnet
+	//+kubebuilder:validation:Optional
+	SouthSubnet string `json:"southSubnet,omitempty"`
+	//ASN
+	//+kubebuilder:validation:Optional
+	ASN uint64 `json:"asn,omitempty"`
+	//Role referring to switch's role: leaf or spine
+	//+kubebuilder:validation:Optional
+	//+kubebuilder:validation:Enum=Leaf;Spine;Undefined
+	Role string `json:"role,omitempty"`
+	//Interfaces referring to details about network interfaces
+	//+kubebuilder:validation:Optional
+	Interfaces []*InterfaceSpec `json:"interfaces,omitempty"`
+	//ScanPorts flag determining whether scanning of ports is requested
+	//+kubebuilder:validation:Optional
+	//+kubebuilder:default=true
+	ScanPorts bool `json:"scanPorts,omitempty"`
 }
 
-// NeighbourSpec defines switch's neighbour
-// +kubebuilder:object:generate=true
-type NeighbourSpec struct {
-	// ID referring to neighbour id
-	// +kubebuilder:validation:Required
-	//todo: +kubebuilder:validation:Pattern=`^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})$`
-	ID string `json:"id"`
-	// Name referring to neighbour name
-	// +kubebuilder:validation:Required
-	Name string `json:"name"`
-	// Type referring to neighbour machine type
-	// +kubebuilder:validation:Required
-	// +kubebuilder:validation:Enum=Machine;Switch
-	Type string `json:"type"`
-	// Port referring to neighbour port name
-	// +kubebuilder:validation:Required
-	Port string `json:"port"`
-	// MACAddress referring to neighbour MAC address
-	// +kubebuilder:validation:Optional
+//LocationSpec defines location details
+//+kubebuilder:object:generate=true
+type LocationSpec struct {
+	//Room referring to room name
+	//+kubebuilder:validation:Optional
+	Room string `json:"room,omitempty"`
+	//Row referring to row number
+	//+kubebuilder:validation:Optional
+	Row int16 `json:"row,omitempty"`
+	//Rack referring to rack number
+	//+kubebuilder:validation:Optional
+	Rack int16 `json:"rack,omitempty"`
+	//HU referring to height in units
+	//+kubebuilder:validation:Optional
+	HU int16 `json:"hu,omitempty"`
+}
+
+//SwitchDistroSpec defines switch OS details
+//+kubebuilder:object:generate=true
+type SwitchDistroSpec struct {
+	//OS referring to switch operating system
+	//+kubebuilder:validation:Optional
+	OS string `json:"os,omitempty"`
+	//Version referring to switch OS version
+	//+kubebuilder:validation:Optional
+	Version string `json:"version,omitempty"`
+	//ASIC
+	//+kubebuilder:validation:Optional
+	ASIC string `json:"asic,omitempty"`
+}
+
+//SwitchChassisSpec defines switch chassis details
+//+kubebuilder:object:generate=true
+type SwitchChassisSpec struct {
+	//Manufacturer referring to switch chassis manufacturer
+	//+kubebuilder:validation:Optional
+	Manufacturer string `json:"manufacturer,omitempty"`
+	//SKU
+	//+kubebuilder:validation:Optional
+	SKU string `json:"sku,omitempty"`
+	//Serial referring to switch chassis serial number
+	//+kubebuilder:validation:Optional
+	Serial string `json:"serial,omitempty"`
+	//ChassisID referring to switch chassis ID advertising via LLDP
+	//+kubebuilder:validation:Optional
+	ChassisID string `json:"chassisId,omitempty"`
+}
+
+//InterfaceSpec defines switch's network interface details
+//+kubebuilder:object:generate=true
+type InterfaceSpec struct {
+	//Lane referring to what lanes are used by the interface
+	//+kubebuilder:validation:Optional
+	Lane string `json:"lane,omitempty"`
+	//FEC referring to error correction method
+	//+kubebuilder:validation:Optional
+	//+kubebuilder:validation:Enum=No;Standard;RS
+	FEC string `json:"fec,omitempty"`
+	//Neighbour referring to neighbour type
+	//+kubebuilder:validation:Optional
+	//+kubebuilder:validation:Enum=Machine;Switch
+	Neighbour string `json:"neighbour,omitempty"`
+	//Name referring to interface's name
+	//+kubebuilder:validation:Optional
+	Name string `json:"name,omitempty"`
+	//MACAddress referring to interface's MAC address
+	//+kubebuilder:validation:Optional
 	MACAddress string `json:"macAddress,omitempty"`
+	//IPv4 referring to interface's IPv4 address
+	//+kubebuilder:validation:Optional
+	IPv4 string `json:"ipv4,omitempty"`
+	//IPv6 referring to interface's IPv6 address
+	//+kubebuilder:validation:Optional
+	IPv6 string `json:"ipv6,omitempty"`
+	//LLDPSystemName
+	//+kubebuilder:validation:Optional
+	LLDPSystemName string `json:"lldpSystemName,omitempty"`
+	//LLDPChassisID
+	//+kubebuilder:validation:Optional
+	LLDPChassisID string `json:"lldpChassisId,omitempty"`
+	//LLDPPortID
+	//+kubebuilder:validation:Optional
+	LLDPPortID string `json:"lldpPortId,omitempty"`
+	//LLDPPortDescription
+	//+kubebuilder:validation:Optional
+	LLDPPortDescription string `json:"lldpPortDescription,omitempty"`
 }
 
 // SwitchStatus defines the observed state of Switch
-type SwitchStatus struct {
-}
+type SwitchStatus struct{}
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
-//+kubebuilder:printcolumn:name="Partition",type=string,JSONPath=`.spec.partition`,description="Partition name"
-//+kubebuilder:printcolumn:name="Room",type=string,JSONPath=`.spec.room`,description="Room name"
-//+kubebuilder:printcolumn:name="Row",type=integer,JSONPath=`.spec.row`,description="Row number"
-//+kubebuilder:printcolumn:name="Rack",type=integer,JSONPath=`.spec.rack`,description="Rack number"
-//+kubebuilder:printcolumn:name="Ports",type=integer,JSONPath=`.spec.ports`,description="Total amount of hardware network interfaces"
-//+kubebuilder:printcolumn:name="Neighbours",type=integer,JSONPath=`.spec.neighboursCount`,description="Total amount of neighbours"
+//+kubebuilder:printcolumn:name="Hostname",type=string,JSONPath=`.spec.hostname`,description="Switch's hostname"
+//+kubebuilder:printcolumn:name="Role",type=string,JSONPath=`.spec.role`,description="switch's role"
+//+kubebuilder:printcolumn:name="OS",type=string,JSONPath=`.spec.switchDistro.os`,description="OS running on switch"
+//+kubebuilder:printcolumn:name="SwitchPorts",type=integer,JSONPath=`.spec.switchPorts`,description="Total amount of non-management network interfaces"
+//+kubebuilder:printcolumn:name="SouthSubnet",type=string,JSONPath=`.spec.southSubnet`,description="South subnet"
+//+kubebuilder:printcolumn:name="ASN",type=integer,JSONPath=`.spec.asn`,description="ASN"
+//+kubebuilder:printcolumn:name="ScanPorts",type=boolean,JSONPath=`.spec.scanPorts`,description="Request for scan ports"
 
 // Switch is the Schema for the switches API
 type Switch struct {
