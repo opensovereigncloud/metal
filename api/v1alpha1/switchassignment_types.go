74d3e3230ec34e491331745c0614703791ad64d1
Artem Bortnikov <aobort@users.noreply.github.com>
2021-05-28 17:42:44 +0300

    add validation webhook for SwitchAssignment CR & tests

--- api/v1alpha1/switchassignment_types.go
+++ api/v1alpha1/switchassignment_types.go
@@ -31,6 +31,7 @@ type SwitchAssignmentSpec struct {
 	Serial string `json:"serial"`
 	// ChassisID refers to switch chassis id
 	//+kubebuilder:validation:Required
+	//+kubebuilder:validation:Pattern='^([0-9a-fA-F]{2}[:]){5}([0-9a-fA-F]{2})$'
 	ChassisID string `json:"chassisId"`
 }
 
--- api/v1alpha1/switchassignment_webhook.go
+++ api/v1alpha1/switchassignment_webhook.go
@@ -19,6 +19,11 @@ package v1alpha1
 import (
 	"strings"
 
+	"github.com/pkg/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -37,7 +42,7 @@ func (swa *SwitchAssignment) SetupWebhookWithManager(mgr ctrl.Manager) error {
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
-//+kubebuilder:webhook:path=/mutate-switch-onmetal-de-onmetal-de-v1alpha1-switchassignment,mutating=true,failurePolicy=fail,sideEffects=None,groups=switch.onmetal.de,resources=switchassignments,verbs=create;update,versions=v1alpha1,name=mswitchassignment.kb.io,admissionReviewVersions={v1,v1beta1}
+//+kubebuilder:webhook:path=/mutate-switch-onmetal-de-v1alpha1-switchassignment,mutating=true,failurePolicy=fail,sideEffects=None,groups=switch.onmetal.de,resources=switchassignments,verbs=create;update,versions=v1alpha1,name=mswitchassignment.kb.io,admissionReviewVersions={v1,v1beta1}
 
 var _ webhook.Defaulter = &SwitchAssignment{}
 
@@ -47,4 +52,69 @@ func (swa *SwitchAssignment) Default() {
 	swa.Labels = map[string]string{}
 	swa.Labels[util.LabelSerial] = swa.Spec.Serial
 	swa.Labels[util.LabelChassisId] = strings.ReplaceAll(swa.Spec.ChassisID, ":", "-")
+}
+
+// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
+//+kubebuilder:webhook:path=/validate-switch-onmetal-de-v1alpha1-switchassignment,mutating=false,failurePolicy=fail,sideEffects=None,groups=switch.onmetal.de.onmetal.de,resources=switchassignments,verbs=create;update,versions=v1alpha1,name=vswitchassignment.kb.io,admissionReviewVersions={v1,v1beta1}
+
+var _ webhook.Validator = &SwitchAssignment{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (swa *SwitchAssignment) ValidateCreate() error {
+	switchAssignmentLog.Info("validate create", "name", swa.Name)
+
+	var allErrors field.ErrorList
+
+	if swa.Spec.Role != util.CSpineRole {
+		allErrors = append(allErrors, field.Invalid(field.NewPath("spec.role"), swa.Spec.Role, "Only spine role can be assigned"))
+	}
+
+	if len(allErrors) > 0 {
+		return apierrors.NewInvalid(schema.GroupKind{
+			Group: GroupVersion.Group,
+			Kind:  "SwitchAssignment",
+		}, swa.Name, allErrors)
+	}
+
+	return nil
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (swa *SwitchAssignment) ValidateUpdate(old runtime.Object) error {
+	switchAssignmentLog.Info("validate update", "name", swa.Name)
+
+	oldSwitchAssignment, ok := old.(*SwitchAssignment)
+	if !ok {
+		return errors.New("failed to cast previous object version to SwitchAssignment resource type")
+	}
+
+	var allErrors field.ErrorList
+
+	if oldSwitchAssignment.Spec.Role != swa.Spec.Role {
+		allErrors = append(allErrors, field.Invalid(field.NewPath("spec.role"), swa.Spec.Role, "Role change disallowed"))
+	}
+
+	if oldSwitchAssignment.Spec.Serial != swa.Spec.Serial {
+		allErrors = append(allErrors, field.Invalid(field.NewPath("spec.serial"), swa.Spec.Serial, "Serial number change disallowed"))
+	}
+
+	if oldSwitchAssignment.Spec.ChassisID != swa.Spec.ChassisID {
+		allErrors = append(allErrors, field.Invalid(field.NewPath("spec.chassisId"), swa.Spec.ChassisID, "Chassis ID change disallowed"))
+	}
+
+	if len(allErrors) > 0 {
+		return apierrors.NewInvalid(schema.GroupKind{
+			Group: GroupVersion.Group,
+			Kind:  "SwitchAssignment",
+		}, swa.Name, allErrors)
+	}
+
+	return nil
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (swa *SwitchAssignment) ValidateDelete() error {
+	switchAssignmentLog.Info("validate delete", "name", swa.Name)
+
+	return nil
 }
