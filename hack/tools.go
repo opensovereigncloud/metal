6562b6156951f456311016756f9ef285ecd33744
Damyan Yordanov <damyan@users.noreply.github.com>
2024-02-29 11:12:31 +0100

    Feature/reuse comliance (#455)
    * Add reuse skeleton – license headers, make targets, etc.
    
    * Update licenses, bulk configuration in .reuse
    
    * Add action for updating the reuse status badge

--- hack/tools.go
+++ hack/tools.go
@@ -1,7 +1,8 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: Apache-2.0
+
 //go:build tools
 
-// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
-// SPDX-License-Identifier: Apache-2.0
 
 package hack
 
